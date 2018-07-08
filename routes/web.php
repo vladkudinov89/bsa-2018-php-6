f4540442179898dcfc90061eddce610e6c73629b
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-09 01:05:21 +0300

    Work#4. Add view parse currencies

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin' , 'namespace' => 'Admin'] , function (){
+
+    Route::get('/currencies' , 'CurrenciesController@index');
+
+});
+
